678c15cb228543ccd2ce826e38f4d9cd93c848a8
lolaop5047 <kthh9828@naver.com>
2025-03-13 11:19:15 +0900

    ⚡️ Add: “ItemData_1.0v”
    ConsumableType 설정
    체력, 배고픔, 스태미나
    
    ItemType 설정
    장착 아이템
    소비 "
    자원 "
    
    canStack = 다중 보유 유무
    maxStackAmount = 최대 보유 수량

--- Assets/02. Scripts/Item/ItemData.cs
+++ Assets/02. Scripts/Item/ItemData.cs
@@ -1,12 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public enum ItemType
 {
+    Equipable, //장착 아이템
+    Consumable, //소비 아이템
+    Resource, //자원 아이템
+}
 
+public enum ConsumableType
+{
+    Health, //체력
+    Hunger, //배고픔
+    Stamina, //스태미나
+
+}
+
+[Serializable]
+
+public class ItemDataConsumabale
+{
+    public ConsumableType type; //타입 설정
+    public float value; //아이템 사용 후 증가 값
 }
 public class ItemData : ScriptableObject
 {
+    [Header("Info")]
+    public string ItemName; //아이템 이름
+    public string description; //아이템 설명
+    public ItemType type; //아이템 타입
+    public Sprite icon; //아이템 아이콘
+    public GameObject dropPrefab; //드롭했을 때 나타나는 3D 오브젝트
 
+    [Header("QuantityLimit")]
+    public bool canStack; //다중 보유가능 유무 
+    public int maxStackAmount; //최대 보유수량
+
+    [Header("Consumable")]
+    public ItemDataConsumabale[] consumabale; //아이템 효과값 배열로 만듬
 }
