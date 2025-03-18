e043c73684a47bbc6a9e2e55e868b5e28c22c484
Punksoda <zug3049@gmail.com>
2025-03-18 15:40:27 +0900

    ✨ Add " Start Scene UI, Runable"
    Add features in StartScene

--- Assets/02. Scripts/UI/StartSceneUI.cs
+++ Assets/02. Scripts/UI/StartSceneUI.cs
@@ -3,9 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class StartSceneUI : MonoBehaviour
 {
+    public GameObject optionUI;
    public void OnClickEnter()
     {
         SceneManager.LoadScene("SihoonScene");
+    }
+
+    public void OnClickOption()
+    {
+        optionUI.SetActive(!optionUI.activeSelf); // activeSelf를 활용하여 켜져 있는지 꺼져 있는지 확인함 
     }
 
     public void OnClickExit()
